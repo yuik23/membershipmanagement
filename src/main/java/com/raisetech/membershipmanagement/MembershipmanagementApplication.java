ee9f5fbccc5428fcd68644ce3c379855eee828f7
yuik23 <yui_karamatsu@yahoo.co.jp>
2024-04-01 09:43:42 +0900

    プロジェクトのセットアップ

---
+++ src/main/java/com/raisetech/membershipmanagement/MembershipmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.raisetech.membershipmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembershipmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MembershipmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raisetech/membershipmanagement/MembershipmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raisetech.membershipmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembershipmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
